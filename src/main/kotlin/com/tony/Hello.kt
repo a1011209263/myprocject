d71d6131830d256d74430888e56c3b24302f5a5d
alex <a1011209263@gmail.com>
2018-12-23 14:56:49 +0800

    Initial commit

---
+++ src/main/kotlin/com/tony/Hello.kt
@@ -0,0 +1,14 @@
+package com.tony
+
+fun main(args: Array<String>) {
+//    println("Hello World" )
+    Human().hello();
+}
+
+class Human{
+
+        fun hello(){
+        println("hello kotaln")
+    }
+
+}
